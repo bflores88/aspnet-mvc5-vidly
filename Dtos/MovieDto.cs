06c736800e242c14a2959f9fd109aa14a2ef89bd
kmietotwy <kmietotwy@gmail.com>
2019-12-18 08:31:18 -0800

    building RESTful services for movies and customers with ASP.NET Web API

--- Dtos/MovieDto.cs
+++ Dtos/MovieDto.cs
@@ -1,32 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
 
-namespace Vidly.Models
+namespace Vidly.Dtos
 {
-    public class Movie
+    public class MovieDto
     {
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        
-        public Genre Genre { get; set; }
         [Required]
         public byte GenreId { get; set; }
-
-
-        [Display( Name = "Release Date" )]
-  
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
-
-        [Display( Name = "Number in Stock" )]
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
-
-
 }
