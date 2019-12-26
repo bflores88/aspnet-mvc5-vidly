be42a47f41a28c4a625b42b6d7e05a744e1e731a
kmietotwy <kmietotwy@gmail.com>
2019-12-26 15:37:51 -0800

    installed toastr for toast notifications.  created jquery validations for input.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -14,6 +14,7 @@ namespace Vidly
                         "~/Scripts/bootbox.js",
                         "~/Scripts/datatables/jquery.datatables.js",
                         "~/Scripts/datatables/datatables.bootstrap.js",
+                        "~/Scripts/toastr.js",
                         "~/Scripts/typeahead.bundle.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -28,6 +29,7 @@ namespace Vidly
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
+                      "~/Content/toastr.css",
                       "~/Content/site.css"));
         }
     }
